38e87151bfdfed7f538db5cab8e1fc9407b83223
Eladio Feijoo <eladio.feijoo@gmal.com>
2024-12-03 13:00:18 -0400

    add customers and invoices pages file

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function CustomersPage() {
+  return (
+    <h1>Customers</h1>
+  )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function InvoicesPage() {
+  return (
+    <h1>Invoices</h1>
+  )
+}
