a9d2e223c0b3fd20454b225f2816b76345977480
Eladio Feijoo <eladio.feijoo@gmal.com>
2024-12-03 12:47:21 -0400

    Ass styles and fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Lusitana} from 'next/font/google';
+import {Montserrat} from 'next/font/google';
+
+export const lusitana = Lusitana({subsets: ['latin'], weight: '400'});
+export const montserrat = Montserrat({subsets: ['latin']});
