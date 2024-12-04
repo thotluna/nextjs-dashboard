5c8a90524125149df51525419867b1239842d2a0
Eladio Feijoo <eladio.feijoo@gmal.com>
2024-12-03 23:12:02 -0400

    add table to invoces page

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -95,7 +95,7 @@ export async function fetchFilteredInvoices(
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
-    const invoices = await sql<InvoicesTable>`
+    const invoices = await client.sql<InvoicesTable>`
       SELECT
         invoices.id,
         invoices.amount,
